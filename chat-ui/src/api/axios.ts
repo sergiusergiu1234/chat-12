048b5907c33b3ef2432747e8bec919e31bc958cb
STEfAN SERGIU <sergiuc.stefan@gmail.com>
2024-02-15 17:59:43 +0200

    update

--- chat-ui/src/api/axios.ts
+++ chat-ui/src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://chats-12-7c193ee8b202.herokuapp.com/'
 
 export default axios.create({
     baseURL: `${BASE_URL}`
